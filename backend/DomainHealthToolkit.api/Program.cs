ac732242807c6e772a1346f5bf745e0760c613ad
JemScripts <jemtekin@gmail.com>
2026-05-16 13:20:48 +0100

    Fixed URL spelling error

--- backend/DomainHealthToolkit.api/Program.cs
+++ backend/DomainHealthToolkit.api/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://www.jemtekin.com")
+            .WithOrigins("https://www.jemtekin.com")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
